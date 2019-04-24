5fdf3fd88b02a5d9b661b96f28261dafed307c2d
simolex <simolex@yandex.ru>
2019-04-24 15:57:07 +0300

    work 20190424_1

--- contracts/DirectoryService.php
+++ contracts/DirectoryService.php
@@ -1,2 +1,12 @@
 <?php
 namespace Salxig\Fias\Contracts;
+
+interface DirectoryService
+{
+	/**
+     * Создает папку и все родительские.
+     *
+     * @return bool
+     */
+	public function create(): bool;
+}
